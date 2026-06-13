6f239d545716bf8c298f3bbc2d2cb01b3004381f
juen1455 <alexraton132@gmail.com>
2026-06-12 23:00:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,6 +63,8 @@ async function enviarAlertaDiscord(id, nombre) {
 async function iniciarEscaneo() {
     const idInicial = obtenerUltimoId();
     const idFinal = idInicial + CANTIDAD_A_ESCANEAR;
+
+    await enviarAlertaDiscord(idInicial, "TEST DE CONEXIÓN - EL BOT ESTÁ VIVO");
     
     console.log(`=== Iniciando escaneo automático: Desde ${idInicial} hasta ${idFinal} ===`);
     
